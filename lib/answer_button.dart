22874856e538c6b56a792ce4c04d8f75966821a3
PedroBaeza0802 <pedro-baeza0802@gmail.com>
2025-06-05 18:40:37 -0600

    Cambio de tipo de botón

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -10,7 +10,7 @@ class AnswerButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onTap,
-      style: ElevatedButton.styleFrom(
+      style: FloatingActionButton.styleFrom(
         padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
           backgroundColor:const Color.fromARGB(255, 33, 1, 95),
           foregroundColor: Colors.white,
